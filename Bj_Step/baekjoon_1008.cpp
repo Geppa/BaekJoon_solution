eedf47baf7515c45b4ca5627b1c266d3faebac81
Geppa <asdjklfgh97@naver.com>
2021-07-29 14:41:53 +0900

    baekjoon_1008

---
+++ Bj_Step/baekjoon_1008.cpp
@@ -0,0 +1,15 @@
+//c++로 입력된 코드입니다.
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	long double A,B;
+    cout<<fixed;
+    cout.precision(9);
+	
+    cin>>A>>B;
+	cout<<A/B;
+
+	return 0;
+}
