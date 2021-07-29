f8df6e4310f2fd9c1ffb6a0668096a42af6807c8
Geppa <asdjklfgh97@naver.com>
2021-07-29 16:21:31 +0900

    baekjoon_15552_c_version

---
+++ Bj_Step/baekjoon_15552.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int numLoof;
+	scanf("%d",&numLoof);
+ 	for(int i=0;i<numLoof;i++)
+ 	{
+ 		int a,b;
+ 		scanf("%d %d",&a,&b);
+ 		printf("%d\n",a+b);
+ 		
+ 	}
+ 
+ 	return 0;
+ }
